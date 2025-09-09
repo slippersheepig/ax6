c92fe90652224223985eaeda96cdf626e9a95332
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2025-09-09 17:03:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,6 @@ rm -rf feeds/luci/applications/luci-app-homeproxy/
 rm -rf feeds/luci/applications/luci-app-adguardhome/
 git clone https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
 git clone https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
+
+# 删除 luci-app-adguardhome 的 init 脚本，避免与 adguardhome 冲突
+rm -f package/luci-app-adguardhome/root/etc/init.d/adguardhome
