3ac423c737eaa8e77c3d628addec1b1932ee275b
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2025-09-09 19:15:41 +0800

    revert

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,11 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
-# 更新 adguardhome：删除原有版本并克隆最新版
-echo "==> 更新 adguardhome 到最新版本"
-rm -rf feeds/packages/net/adguardhome
-git clone https://github.com/immortalwrt/packages.git tmp-adguardhome
-cd tmp-adguardhome
-mv net/adguardhome ../feeds/packages/net/
-cd ..
-rm -rf tmp-adguardhome
