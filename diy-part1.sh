812be7fcdb3a49df7bb9bb2cc3afcf896462dbaa
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2025-10-16 14:52:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,11 +16,12 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-rm -rf feeds/luci/applications/luci-app-homeproxy/
-rm -rf feeds/luci/applications/luci-app-adguardhome/
+rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/luci/applications/luci-app-adguardhome
+rm -rf feeds/luci/applications/luci-app-homeproxy
 git clone --depth 1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
-# 从 kenzok8 仓库只取 luci-app-adguardhome
-echo "==> 使用 kenzok8 的 luci-app-adguardhome"
+echo "==> 使用 kenzok8 的 adguardhome"
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages tmp-kenzo
 mv tmp-kenzo/luci-app-adguardhome package/
+mv tmp-kenzo/adguardhome package/
 rm -rf tmp-kenzo
