830df814d4175358aa588b44a4805d12b29dfe35
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2025-10-17 16:53:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,20 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
-
-rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/luci/applications/luci-app-adguardhome
-rm -rf feeds/luci/applications/luci-app-homeproxy
-git clone --depth 1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
-echo "==> 使用 kenzok8 的 adguardhome"
-git clone --depth 1 https://github.com/kenzok8/openwrt-packages tmp-kenzo
-mv tmp-kenzo/luci-app-adguardhome package/
-mv tmp-kenzo/adguardhome package/
-rm -rf tmp-kenzo
-echo "==> 更新 sing-box 到最新版本"
-rm -rf feeds/packages/net/sing-box
-git clone --depth 1 https://github.com/immortalwrt/packages tmp-sing-box
-cd tmp-sing-box
-mv net/sing-box ../feeds/packages/net/
-cd ..
-rm -rf tmp-sing-box
