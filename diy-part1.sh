2407e6949c2e558c9b5e8d6bb86276f9d52f6199
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2025-10-17 20:20:52 +0800

    revert

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,67 +16,11 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# ==> 清理 feeds 中的默认软件包定义，防止冲突
-echo "==> Removing default packages from feeds to prevent conflicts..."
-rm -rf feeds/luci/applications/luci-app-adguardhome
-rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/luci/applications/luci-app-homeproxy
-rm -rf feeds/packages/net/sing-box
-
-# ==> 添加自定义软件包
-echo "==> Adding custom packages..."
-
-# 1. 添加 immortalwrt 的 HomeProxy (这是一个独立仓库，直接 clone 即可)
-echo "Cloning luci-app-homeproxy..."
-git clone --depth=1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
-
-# 2. 使用 sparse-checkout 添加 kenzok8 的 luci-app-adguardhome
-echo "Cloning AdGuardHome packages from kenzok8..."
-PKG_REPO_URL="https://github.com/kenzok8/openwrt-packages"
-PKG_BRANCH="master"
-# 需要拉取的软件包文件夹
-PKG_DIRS="luci-app-adguardhome"
-
-# 创建临时目录并进行 sparse checkout
-git clone --depth 1 --no-checkout --filter=blob:none -b "$PKG_BRANCH" "$PKG_REPO_URL" kenzok8-packages
-cd kenzok8-packages
-git sparse-checkout init --cone
-git sparse-checkout set $PKG_DIRS
-git checkout "$PKG_BRANCH"
-# 将拉取到的文件移动到 package 目录
-mv -f $PKG_DIRS ../package/
-cd ..
-rm -rf kenzok8-packages
-
-# 3. 使用 sparse-checkout 添加 immortalwrt 的 AdGuard Home
-echo "Cloning latest adguardhome from immortalwrt/packages..."
-PKG_REPO_URL="https://github.com/immortalwrt/packages"
-PKG_BRANCH="master"
-PKG_DIRS="net/adguardhome"
-
-git clone --depth 1 --no-checkout --filter=blob:none -b "$PKG_BRANCH" "$PKG_REPO_URL" immortalwrt-packages
-cd immortalwrt-packages
-git sparse-checkout init --cone
-git sparse-checkout set $PKG_DIRS
-git checkout "$PKG_BRANCH"
-mv -f $PKG_DIRS ../package/
-cd ..
-rm -rf immortalwrt-packages
-
-# 4. 使用 sparse-checkout 添加 immortalwrt 的最新版 sing-box
-echo "Cloning latest sing-box from immortalwrt/packages..."
-PKG_REPO_URL="https://github.com/immortalwrt/packages"
-PKG_BRANCH="master"
-PKG_DIRS="net/sing-box"
-
-git clone --depth 1 --no-checkout --filter=blob:none -b "$PKG_BRANCH" "$PKG_REPO_URL" immortalwrt-packages
-cd immortalwrt-packages
-git sparse-checkout init --cone
-git sparse-checkout set $PKG_DIRS
-git checkout "$PKG_BRANCH"
-mv -f $PKG_DIRS ../package/
-cd ..
-rm -rf immortalwrt-packages
-
-echo "All custom packages added successfully."
+rm -rf feeds/luci/applications/luci-app-homeproxy/
+rm -rf feeds/luci/applications/luci-app-adguardhome/
+git clone --depth 1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
+# 从 kenzok8 仓库只取 luci-app-adguardhome
+echo "==> 使用 kenzok8 的 luci-app-adguardhome"
+git clone --depth 1 https://github.com/kenzok8/openwrt-packages tmp-kenzo
+mv tmp-kenzo/luci-app-adguardhome package/
+rm -rf tmp-kenzo
